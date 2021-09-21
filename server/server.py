ccec4a1c1ff217ceaf53c72de07cfbc4d6d4cf8f
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-21 15:05:47 +0100

    corrected error in importing request in server file

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, require, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
