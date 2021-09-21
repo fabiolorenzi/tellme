261954e394baf84cad2342c9c46fba7c8a12292f
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-21 14:58:09 +0100

    created model for user in server file

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from cryptography.fernet import Fernet
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,34 @@ db = SQLAlchemy(app)
 ma = Marshmallow(db)
 
 #-------------------------MODELS-------------------------
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25))
+    name = db.Column(db.String(25))
+    surname = db.Column(db.String(30))
+    birthday = db.Column(db.Date)
+    subscribed_since = db.Column(db.Date)
+    city = db.Column(db.String(30))
+    email = db.Column(db.String(40))
+    password = db.Column(db.String(20))
+
+    def __init__(self, username, name, surname, birthday, subscribed_since, city, email, password):
+        self.username = username
+        self.name = name
+        self.surname = surname
+        self.birthday = birthday
+        self.subscribed_since = subscribed_since
+        self.city = city
+        self.email = email
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "name", "surname", "birthday", "subscribed_since", "city", "email", "password")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 #-------------------------ROUTES-------------------------
 
