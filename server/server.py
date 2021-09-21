73e0caf3348d59b0e42ebf037109b7f377bdbda3
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-21 14:50:30 +0100

    updated imports in server file

--- server/server.py
+++ server/server.py
@@ -2,10 +2,7 @@ from flask import Flask, require, jsonify
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-import base64
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 CORS(app)
