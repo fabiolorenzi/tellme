d851ed0a49a705b62ada728cfac87d2f42b4af1d
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-21 14:41:38 +0100

    connected server to db

--- server/server.py
+++ server/server.py
@@ -10,6 +10,12 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 app = Flask(__name__)
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:sVChIQwU.wqjqsM7@localhost/tellme"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(db)
+
 #-------------------------MODELS-------------------------
 
 #-------------------------ROUTES-------------------------
