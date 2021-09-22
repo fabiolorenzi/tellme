e9f96cb4325d8ec55512d9af4f038be0665b4a3d
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-22 20:11:37 +0100

    imported and setted dependencies in app.modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
