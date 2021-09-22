ce85439c7f9ac64ea16b9efffe4fe494b2bf2515
fabiolorenzi <fabiolorenzi@gmail.com>
2021-09-22 20:26:51 +0100

    resetted client folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LeftPartComponent } from './components/left-part/left-part.component';
-import { HorizontalMenuComponent } from './components/horizontal-menu/horizontal-menu.component';
 
 const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LeftPartComponent,
-    HorizontalMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
